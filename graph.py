f8cf8cf1165bfbc3bf7de90424df198052847026
am8103 <am8103@node-1.babel-am8103.ch-geni-net.genirack.nyu.edu>
2016-08-16 15:56:24 -0400

    Code to generate colored graph and subgraph

---
+++ graph.py
@@ -0,0 +1,26 @@
+import community
+import networkx as nx
+import matplotlib.pyplot as plt
+import seaborn as sns
+G = nx.Graph(nx.drawing.nx_pydot.read_dot("topology.dot.plain"))
+partition = community.best_partition(G)
+colors = sns.color_palette("hls", max(partition.values())+1)
+#drawing
+size = float(len(set(partition.values())))
+pos = nx.spring_layout(G)
+for com in set(partition.values()) :
+    list_nodes = [nodes for nodes in partition.keys()
+                                if partition[nodes] == com]
+    nx.draw_networkx_nodes(G, pos, list_nodes, node_size = 30,
+                                node_color = colors[com])
+
+
+nx.draw_networkx_edges(G,pos, alpha=0.5, width=0.5)
+plt.savefig("Color-Graph.png", format="PNG")
+
+plt.clf()
+
+nodebunch = [node for node in G.nodes() if partition[node]==3]
+H = G.subgraph(nodebunch)
+nx.draw_spring(H, node_size = 30, node_color = colors[4])
+plt.savefig("subgraph-3.png", format="PNG")
